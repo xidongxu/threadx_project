e15e3b850662f751d1553d3f04c7b7617d90597c
xidongxu <xi_dongxu@126.com>
2022-12-11 20:51:49 +0800

    【修改】 组件目录；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

---
+++ components/Inc/cmd.h
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * @file           : cmd.h
+  * @brief          : Header for cmd.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * nothing to write.
+  *
+  ******************************************************************************
+  */
+
+#ifndef __CMD_H__
+#define __CMD_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CMD_H__ */
---
+++ components/Inc/log.h
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * @file           : log.h
+  * @brief          : Header for log.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * nothing to write.
+  *
+  ******************************************************************************
+  */
+
+#ifndef __LOG_H__
+#define __LOG_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __LOG_H__ */
