db52d8da228645f43cc5f3a31a59b61df7318a79
xidongxu <xi_dongxu@126.com>
2022-12-11 14:22:51 +0800

    【修改】 添加工程构建后执行的脚本文件；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

---
+++ boards/stm32/stm32h750/threadx_app/MDK-ARM/threadx_app_build.cmd
@@ -0,0 +1,4 @@
+@echo off
+move %~dp0threadx_app\threadx_app.map %~dp0build\threadx_app.map
+move %~dp0threadx_app\threadx_app.hex %~dp0build\threadx_app.hex
+move %~dp0threadx_app\threadx_app.axf %~dp0build\threadx_app.axf
