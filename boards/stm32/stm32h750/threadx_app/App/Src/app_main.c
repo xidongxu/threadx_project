9c690f8425a4c43e83efc95861eccdc3a2ecf23d
xidongxu <xi_dongxu@126.com>
2022-12-11 16:19:00 +0800

    【修改】 用户应用；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

---
+++ boards/stm32/stm32h750/threadx_app/App/Src/app_main.c
@@ -0,0 +1,45 @@
+/**
+  ******************************************************************************
+  * @file           : user_main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * nothing to write.
+  ******************************************************************************
+  */
+  
+#include "app_main.h"
+#include "tx_api.h"
+
+static uint8_t thread1_stack[1024];
+static uint8_t thread2_stack[1024];
+static TX_THREAD thread1;
+static TX_THREAD thread2;
+
+void thread1_entry(ULONG thread_input)
+{
+	/* Enter into a forever loop. */
+	while(1)
+	{
+    HAL_GPIO_WritePin(GPIOI, GPIO_PIN_8, GPIO_PIN_SET);
+		tx_thread_sleep(700);
+    HAL_GPIO_WritePin(GPIOI, GPIO_PIN_8, GPIO_PIN_RESET);
+    tx_thread_sleep(300);
+	}
+}
+
+void thread2_entry(ULONG thread_input)
+{
+	while(1)
+	{
+		tx_thread_sleep(100);
+	}
+}
+
+void tx_application_define(void *first_unused_memory)
+{
+	/* Create my_thread! */
+	tx_thread_create(&thread1, "Thread1", thread1_entry, 0x1234, thread1_stack, 1024, 3, 3, TX_NO_TIME_SLICE, TX_AUTO_START);
+  tx_thread_create(&thread2, "Thread2", thread2_entry, 0x1234, thread2_stack, 1024, 3, 3, TX_NO_TIME_SLICE, TX_AUTO_START);
+}
--- boards/stm32/stm32h750/threadx_app/Core/Src/main.c
+++ boards/stm32/stm32h750/threadx_app/Core/Src/main.c
@@ -35,10 +35,7 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-static uint8_t thread1_stack[1024];
-static uint8_t thread2_stack[1024];
-static TX_THREAD thread1;
-static TX_THREAD thread2;
+
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -60,32 +57,7 @@ static void MX_UART4_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void thread1_entry(ULONG thread_input)
-{
-	/* Enter into a forever loop. */
-	while(1)
-	{
-    HAL_GPIO_WritePin(GPIOI, GPIO_PIN_8, GPIO_PIN_SET);
-		tx_thread_sleep(700);
-    HAL_GPIO_WritePin(GPIOI, GPIO_PIN_8, GPIO_PIN_RESET);
-    tx_thread_sleep(300);
-	}
-}
 
-void thread2_entry(ULONG thread_input)
-{
-	while(1)
-	{
-		tx_thread_sleep(100);
-	}
-}
-
-void tx_application_define(void *first_unused_memory)
-{
-	/* Create my_thread! */
-	tx_thread_create(&thread1, "Thread1", thread1_entry, 0x1234, thread1_stack, 1024, 3, 3, TX_NO_TIME_SLICE, TX_AUTO_START);
-  tx_thread_create(&thread2, "Thread2", thread2_entry, 0x1234, thread2_stack, 1024, 3, 3, TX_NO_TIME_SLICE, TX_AUTO_START);
-}
 /* USER CODE END 0 */
 
 /**
