ae8b872e485e906b1f5ffce02c5ca432d3ced30b
xidongxu <xi_dongxu@126.com>
2022-12-19 23:13:37 +0800

    【修改】 工程配置；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

--- boards/stm32/stm32h750/threadx_app/App/Src/app_main.c
+++ boards/stm32/stm32h750/threadx_app/App/Src/app_main.c
@@ -13,13 +13,14 @@
 #include "log.h"
 #include "tx_api.h"
 
-static uint8_t thread1_stack[1024];
-static uint8_t thread2_stack[1024];
+static uint8_t thread1_stack[2048];
+static uint8_t thread2_stack[2048];
 static TX_THREAD thread1;
 static TX_THREAD thread2;
 
 void thread1_entry(ULONG thread_input)
 {
+    int count = 0;
 	/* Enter into a forever loop. */
 	while(1)
 	{
@@ -27,6 +28,8 @@ void thread1_entry(ULONG thread_input)
 		tx_thread_sleep(700);
         HAL_GPIO_WritePin(GPIOI, GPIO_PIN_8, GPIO_PIN_RESET);
         tx_thread_sleep(300);
+        printf("hello world - %d\r\n", count);
+        count++;
 	}
 }
 
@@ -45,6 +48,13 @@ void thread2_entry(ULONG thread_input)
 void tx_application_define(void *first_unused_memory)
 {
 	/* Create my_thread! */
-    tx_thread_create(&thread1, "Thread1", thread1_entry, 0x1234, thread1_stack, 1024, 3, 3, TX_NO_TIME_SLICE, TX_AUTO_START);
-    tx_thread_create(&thread2, "Thread2", thread2_entry, 0x1234, thread2_stack, 1024, 3, 3, TX_NO_TIME_SLICE, TX_AUTO_START);
+    tx_thread_create(&thread1, "Thread1", thread1_entry, 0x1234, thread1_stack, sizeof(thread1_stack), 3, 3, TX_NO_TIME_SLICE, TX_AUTO_START);
+    tx_thread_create(&thread2, "Thread2", thread2_entry, 0x1234, thread2_stack, sizeof(thread2_stack), 3, 3, TX_NO_TIME_SLICE, TX_AUTO_START);
 }
+
+#ifndef TX_ENABLE_STACK_CHECKING
+void _tx_thread_stack_error_handler(void)
+{
+
+}
+#endif /* TX_ENABLE_STACK_CHECKING */
--- boards/stm32/stm32h750/threadx_app/Device/Src/uart.c
+++ boards/stm32/stm32h750/threadx_app/Device/Src/uart.c
@@ -13,6 +13,7 @@
 #include "log.h"
 
 #define LOG_OUTPUT_TIMEOUT  (100U)
+#define DATA_INPUT_TIMEOUT  (100U)
 
 extern UART_HandleTypeDef huart4;
 
@@ -24,6 +25,25 @@ static void kputchar(char ch)
         __HAL_UNLOCK(&huart4);
     }
     HAL_UART_Transmit(&huart4, (uint8_t *)&ch, 1, LOG_OUTPUT_TIMEOUT);    
+}
+
+int fputc(int ch, FILE *f)
+{
+    if(huart4.gState != HAL_UART_STATE_READY)
+    {
+        HAL_UART_Abort(&huart4);
+        __HAL_UNLOCK(&huart4);
+    }
+    HAL_UART_Transmit(&huart4, (uint8_t *)&ch, 1, LOG_OUTPUT_TIMEOUT);
+    return (ch);
+}
+
+int fgetc(FILE *f)
+{
+    uint8_t data = 0;
+    while (__HAL_UART_GET_FLAG(&huart4, UART_FLAG_RXNE) == RESET);
+    HAL_UART_Receive(&huart4, &data, 1, DATA_INPUT_TIMEOUT);
+    return (int)data;
 }
 
 int uart_init(void)
